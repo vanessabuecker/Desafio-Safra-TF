e4498cc4e0153fe4e5d65ebce87570d8a54018f9
Vanessa Buecker <v.buecker@rocketmail.com>
2022-12-05 07:48:48 -0300

    cria função para extrato e corrige bug do layout

--- src/pages/credentials/app.js
+++ src/pages/credentials/app.js
@@ -3,49 +3,56 @@ export default () => {
   const template = `
     <div id="container-credentials">
       <div class="header-credentials">
-        <img class="logo-safra" src="../src/img/logo-home.png" alt="Logo Safra">
-        <p class="user-name">Olá, </p>
+        <img class="logo-safra" src="./img/logo-home.png" alt="Logo Safra">
+        <p class="user-name">Olá, Vanessa!</p>
       </div>
-      <div class="instruction-title">
-        <a href="#selectBank"><img id="arrow-icon" src="../src/img/arrow.png" alt="Ícone - seta para a esquerda"/></a>
+      <div class="title-container">
+            <a href="#selectBank">
+                <img id="arrow-icon" src="./img/arrow.png" alt="Ícone - seta para a esquerda">
+            </a>
+            <p class="title-text"> Insira suas credenciais</p> 
+        </div>
+
       </div>
       <div id="form-credentials"
         <form>
-          <p class="titles">Inserir Credenciais</p>
-          <select id="options-credentials">
-            <option disabled selected>Selecione:</option>
-            <option value="">Opção 1</option>
-            <option value="">Opção 2</option>
-            <option value="">Opção 3</option>
-          </select><br>
-          <fieldset class="field">
-            <legend>CPF</legend>
-            <input type="text" id="cpf-credentials" class="pswrd-cpf-credentials"/>
-          </fieldset>
-          <fieldset class="field">
-            <legend>Senha</legend>
-            <input type="password" id="pswrd-credentials" class="pswrd-cpf-credentials"/>
-          </fieldset>
-          <p class="div-error"> </p>
-          <a href="#home"><button id="credentials-ok-conect" class="btns">Vincular Banco</button></a>
-        </form>
-      </div>
-    </div>`;
+       <p class="titles">Inserir Credenciais</p>
+        <select id="options-credentials">
+          <option disabled selected>Selecione:</option>
+          <option value="Cpf">CPF</option>
+          <option value="Nº da conta">Nº da conta</option>
+        </select><br>
+        <fieldset class="field">
+          <legend>CPF</legend>
+          <input type="text" id="cpf-credentials" class="pswrd-cpf-credentials"/>
+        </fieldset>
+        <fieldset class="field">
+          <legend>Senha</legend>
+          <input type="password" id="pswrd-credentials" class="pswrd-cpf-credentials"/>
+        </fieldset>
+        <p class="div-error"> </p>
+        <button id="credentials-ok-conect" class="btns">Vincular Banco</button>
+      </form>
+    </div>
+    <div id="modal-ok-bank" class="hide">
+      <p>instituição vinculada com sucesso!</p>
+      <button id="link-ok">OK</button>
+    </div>
+  </div>`;
 
   container.innerHTML = template;
 
   const linkBank = container.querySelector('#credentials-ok-conect');
-  const cpfCredentials = container.querySelector('#cpf-credentials');
-  const passwordCredentials = container.querySelector('#pswrd-credentials');
-  const divErro = container.querySelector('.div-error');
-
+  const modal = container.querySelector('#modal-ok-bank');
+  const modalOkBtn = container.querySelector('#link-ok');
+  
   linkBank.addEventListener('click', () => {
-    if(cpfCredentials.value === '' || passwordCredentials.value === '') {
-      divErro.innerHTML = 'Por favor, preencha todos os campos.'
-    } else {
-      window.location.hash = '#home'
-    }
-  });
-
+    modal.classList.toggle('hide');
+  })
+  
+  modalOkBtn.addEventListener('click', () => {
+    window.location.hash = '#home'
+  })
+  
   return container;
 };
--- src/pages/home/app.js
+++ src/pages/home/app.js
@@ -5,88 +5,82 @@ import { auth } from '../../lib/config_firebase.js';
 export default () => {
 
   const container = document.createElement('div');
-  const template = `   <div class="container-homepg">
-    <div class="retangle-home">
-        <div class="logo-home">
-            <img id="logo-safra-home" href='#home' src="./img/logo-home.png" alt="Logo Safra"> <span class="userName">Olá,
-                Fulano!</span>
+  const template = ` <div class="container-homepg">
+  <div class="retangle-home">
+      <div class="logo-home">
+          <img id="logo-safra-home" href='#home' src="./img/logo-home.png" alt="Logo Safra"> <span class="userName">Olá,
+              Fulano!</span>
+      </div>
+      <div class="total-balance">
+        <p class="txt-balance">Saldo disponível</p>
+        <div class="balance">
+             <span class="totalValue"> R$15.300,00</span> <div class="img-eyes"><img id="btn-eyes" src="./img/eyes-icon.png"
+                 alt="Ícone olhos fechados"></div>
         </div>
-        <div class="total-balance">
-            <p class="txt-balance">Saldo disponível</p>
-            <div class="balance">
-                <span class="totalValue"> R$15.300,00</span> 
-            </div> 
-            <div>
-                <img id='btn-eyes' class='icon-eyes' alt='Ícone de olhos para mostrar/ocultar' src="./img/eyes-outline-icon.png">
-            </div>
-  
-            <p class="txt-balance">Lançamentos futuros</p>
-            <div class="balance">
-                <span class="totalValue"> R$...</span> 
-            </div> 
-            <div>
-                <img id='btn-eyes' class='icon-eyes' alt='Ícone de olhos para mostrar/ocultar' src="./img/eyes-outline-icon.png">    
-            </div>
 
-            <div class="txt-balance" id="showExt" >Vizualizar extrato</div>
-        </div>
-        <div class="button-openbanking-add">
-            <a href="#openFinance"> <img id="icon-ob-add" src="./img/icon-open-banking.png"
-                    alt="Ícone open banking cor-de-rosa"> </a> Adicionar Open Finance
-        </div>
-  
-        <div class="container-bank">
-            
-        </div>
-  
-        <div class="pendencies">
-            <span class="pendencies-title"> Pendências <img class="icon-caution" src="./img/caution-icon.png" alt="Ícone de precaução">
-            </span>
-        </div>
-        
-        <div class="pendencies-list">
-             <img class="icon-caution" src="./img/ReactBank.png"
-                alt="Ícone de precaução"> <span id="item-list"> Cartão</span>  <span id="balance-pend"> R$3.560,00</span>
-        </div>
-        <div class="wrapper">  </div>
-    </div>
-  
-    <div class="retangle-bottom-home">
-        <div class="content-bottom-retangle">
-
-            <a href="#home"><img id="icon-home" src="./img/icon-home.png" alt="Ícone HOME" style="width:35px;height:35px;"></a>
-            <img id="icon-pix" src="./img/pix-icon.png" alt="Ícone Pix">
-            <img id="icon-logout" src="./img/logout-icon.png" alt="Ícone de logout">
-  
+        <p class="txt-balance">Lançamentos futuros</p>
+        <div class="balance">
+             <span class="totalValue"> R$...</span> <img id="btn-eyes" src="./img/eyes-icon.png"
+                 alt="Ícone olhos abertos">                    
+        </div> 
+         
+         <div class="txt-balance" id="showExt">
+         <img id="ext-bank" src="./img/folha.png"
+                 alt="Ícone open banking cor-de-rosa">
+        <span class="ext-view"> Vizualizar extrato</span>
         </div>
     </div>
-  
-  </div>`;
+      <div class="button-openbanking-add">
+          <a href="#openFinance"> <img id="icon-ob-add" src="./img/icon-open-banking.png"
+                  alt="Ícone open banking cor-de-rosa"> </a> Adicionar Open Finance
+      </div>
 
-  /*const btnShow = container.querySelector('#btn-eyes');
-  const balance = container.querySelector('.balance');
+      <div class="container-bank">
+          
+      </div>
 
-  btnShow.addEventListener('click', function() {
-    if (balance.style.display === 'block') {
-      balance.style.display = 'none';
-    } else{
-      balance.style.display = 'block';
-    }
+      <div class="pendencies">
+          <span class="pendencies-title"> Pendências <img class="icon-caution" src="./img/caution-icon.png" alt="Ícone de precaução">
+          </span>
+      </div>
+      
+      <div class="pendencies-list">
+           <img class="icon-caution" src="./img/ReactBank.png"
+              alt="Ícone de precaução"> <span id="item-list"> Cartão</span>  <span id="balance-pend"> R$3.560,00</span>
+      </div>
 
-  });*/
+      <div class="talk-to-safira"><a href="https://wa.me/5527997300762?text=Ol%C3%A1.%20Eu%20sou%20a%20Safira!%20Em%20que%20posso%20ajudar?" target="_blank"> <img id="safira-whats" src="./img/safira-whats.png"
+      alt="Ícone open banking cor-de-rosa"> </a> </div>
+      <div class="wrapper">  </div>
+  </div>
 
-  /*const btnShow = container.querySelector('#btn-eyes');
+  <div class="retangle-bottom-home">
+      <div class="content-bottom-retangle">
+          <a href="#home"><img id="icon-home" src="./img/icon-home.png" alt="Ícone HOME"></a>
+          <img id="icon-sino" src="./img/sino.png" alt="Ícone de sino">
+          <img id="icon-logout" src="./img/logout-icon.png" alt="Ícone de logout">
 
-  btnShow.addEventListener('click', function() {
-    const balance = container.querySelector('.balance');
-    balance.classList.toggle("hide");
+      </div>
+  </div>
 
-  });*/
+</div>`;
 
   container.innerHTML = template;
-  const btnLogout = container.querySelector('#icon-logout');
 
-  const btn = container.querySelector('#icon-home');
+  const btnShow = container.querySelector('#btn-eyes');
+  const balance = container.querySelector('.totalValue');
+  const imgEyes = container.querySelector('.img-eyes')
+
+  btnShow.addEventListener('click', function () {
+    if (balance.style.display === 'flex') {
+      balance.style.display = 'none';
+
+    } else {
+      balance.style.display = 'flex';
+    }
+  })
+
+  const btnLogout = container.querySelector('#icon-logout');
   const showExt = container.querySelector('#showExt');
 
   showExt.addEventListener('click', (e) => {
@@ -94,30 +88,107 @@ export default () => {
     const modalContentElement = document.getElementById('modal_content');
     const modalElement = document.getElementById('modal');
     modalElement.classList.add('show-modal');
-
     modalContentElement.innerHTML = `
-            <div class="container-ext">
-            <div class="inst-name">Extrato</div>
-        <div class="box-ext">
+    <div class="container-ext">
+    <div class="inst-name">Extrato</div>
+    <div class="box-ext">
         <label for="period">Selecionar período:</label>
-    <select name="month" id="month" form="monform">
-      <option value="jan-fev">Janeiro - Fevereiro</option>
-      <option value="marc-apr">Março - Abril</option>
-      <option value="may-jun">Maio - Junho</option>
-      <option value="jul-aug">Julho - Agosto</option>
-      <option value="sep-oct">Setembro - Outubro</option>
-      <option value="nov-dec">Novembro - Dezembro</option>
-    </select>
-            <div class="ext"> 
-            <li>Água: - 100,00</li>
-            <li>Energia: -450,00</li>
-            <li>Internet: -100,00</li>
-            <li>Pensão alim.: 800,00</li>
-            
-            </div>
-        </div>
+        <select name="month" id="month" form="monform">
+            <option value="jan-fev">Janeiro - Fevereiro</option>
+            <option value="marc-apr">Março - Abril</option>
+            <option value="may-jun">Maio - Junho</option>
+            <option value="jul-aug">Julho - Agosto</option>
+            <option value="sep-oct">Setembro - Outubro</option>
+            <option value="nov-dec">Novembro - Dezembro</option>
+        </select>
+    </div>
+    <div class="result"> </div>
+</div>`;
+    let select = modalContentElement.querySelector('#month');
+    const boxExt = modalContentElement.querySelector('.result');
 
-    </div>`;
+    select.addEventListener('change', () => {
+      if (select.options[select.selectedIndex].value === 'jan-fev') {
+        boxExt.innerHTML = `   <div class="ext">
+        <li> Invest.Css Resgate Autom. </li>
+        <li class="ext-item-cred"> 100,00</li>
+    </div>
+
+    <div class="ext">
+        <li> Conpra com cartão PADARIA NODE </li>
+        <li class="ext-item-deb"> -100,00</li>
+    </div>`
+      }
+
+      else if (select.options[select.selectedIndex].value === 'marc-apr') {
+        boxExt.innerHTML = `   <div class="ext">
+        <li> Invest.Css Resgate Autom. </li>
+        <li class="ext-item-cred"> 900,00</li>
+    </div>
+
+    <div class="ext">
+        <li> Conta com cartão SUP. SAO HTML </li>
+        <li class="ext-item-deb"> -650,00</li>
+    </div>`
+      }
+      else if (select.options[select.selectedIndex].value === 'may-jun') {
+        boxExt.innerHTML = `   <div class="ext">
+        <li> PIX enviado JOAO DA SILVA </li>
+        <li class="ext-item-cred"> 2.500,00</li>
+    </div>
+
+    <div class="ext">
+        <li> Compra com cartão SUP. MERCADO SAO NODE </li>
+        <li class="ext-item-deb"> -650,00</li>
+    </div>`
+      }
+
+      else if (select.options[select.selectedIndex].value === 'jul-aug') {
+        boxExt.innerHTML = `   <div class="ext">
+        <li> DOC JOSÉ SANTOS </li>
+        <li class="ext-item-cred"> 4.500,50</li>
+    </div>
+
+    <div class="ext">
+        <li> Compra com cartão MERC. SANTA'ANNA JS </li>
+        <li class="ext-item-deb"> -350,00</li>
+    </div>`
+      }
+
+      else if (select.options[select.selectedIndex].value === 'sep-oct') {
+        boxExt.innerHTML = `   <div class="ext">
+        <li> DOC JOSÉ SANTOS </li>
+        <li class="ext-item-cred"> 4.500,00</li>
+    </div>
+
+    <div class="ext">
+        <li> Compra com cartão MERC. SANTA'ANNA JS </li>
+        <li class="ext-item-deb"> -545,00</li>
+    </div>
+    
+    <div class="ext">
+        <li> Compra com cartão MAT. CONST. REACT </li>
+        <li class="ext-item-deb"> -5.650,20</li>
+    </div>`
+      }
+      else if (select.options[select.selectedIndex].value === 'nov-dec') {
+        boxExt.innerHTML = `   <div class="ext">
+        <li> DOC JOSÉ SANTOS </li>
+        <li class="ext-item-cred"> 1.000,00</li>
+    </div>
+
+    <div class="ext">
+        <li> Compra com cartão MERC. SANTA'ANNA JS </li>
+        <li class="ext-item-deb"> -231,10</li>
+    </div>
+    
+    <div class="ext">
+        <li> Compra com cartão PAPELARIA JAVASCRIPT </li>
+        <li class="ext-item-deb"> -52,27</li>
+    </div>`
+      }
+    })
+
   })
 
   const spanClose = document.querySelector('#close');
@@ -136,8 +207,6 @@ export default () => {
     });
   });
 
-
-
   const showBanks = async () => {
 
     const allBanks = await getAllBanks();
@@ -147,26 +216,20 @@ export default () => {
       if (bank.cliente.includes(auth.currentUser.uid) === true) {
         banks = `
         <div class="bank-card">
-          <span class="account-type"> ${bank.instituicao}</span>
+          <span class="bank-name"> ${bank.instituicao}</span>
           <div class="logo-bank">
-            <img id="logo-alfa" src="./img/ReactBank.png" alt="Ícone open banking cor-de-rosa">
-            
             <span class="account-type"> ${bank.tipo}</span>
           </div>
   
           <div class="balance-tamplate">
-            <span class="totalValue" id="total-value-template">${bank.saldo} </span> <img class="icon-eyes-template"
-                src="./img/eyes-off-icon.png" alt="Ícone olhos abertos">
+            <span class="totalValue" id="total-value-template">R$${bank.saldo} 
           </div>
         </div>`;
 
-
         return banks;
 
-       }else{
-        //  banks.style.display = "none";
-         console.log("oi")
-       }
+      } else {
+      }
 
     }).join('');
 
@@ -177,36 +240,8 @@ export default () => {
   showBanks();
 
   return container;
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
